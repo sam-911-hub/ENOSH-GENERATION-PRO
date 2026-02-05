499099381b450a909b74a4d0a605dbc08e3cc93e
sam-911-hub <samreigns71@gmail.com>
2026-02-05 16:15:12 +0300

    npm 12

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './src/index.css';
+// Use the project's main white-theme stylesheet instead of the old Tailwind-based file
+import './css/styles.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
